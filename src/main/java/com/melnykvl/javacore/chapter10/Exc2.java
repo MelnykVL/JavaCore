dbcf3a80e403b9aff520ba84a27a9da269e13117
MelnykVL <w.vladyslavmelnyk@gmail.com>
2021-06-29 11:18:08 +0300

    Exc2, HandleError are done

---
+++ src/main/java/com/melnykvl/javacore/chapter10/Exc2.java
@@ -0,0 +1,19 @@
+package main.java.com.melnykvl.javacore.chapter10;
+
+public class Exc2 {
+    public static void main(String[] args) {
+        int d, a;
+
+        try {
+
+            d = 0;
+            a = 42/d;
+            System.out.println("Hi from try");
+
+        } catch (ArithmeticException e) {
+            System.out.println("Hi from catch");
+        }
+
+        System.out.println("The END");
+    }
+}
---
+++ src/main/java/com/melnykvl/javacore/chapter10/HandleError.java
@@ -0,0 +1,25 @@
+package main.java.com.melnykvl.javacore.chapter10;
+
+import java.util.Random;
+
+public class HandleError {
+    public static void main(String[] args) {
+        int a =  0, b = 0, c = 0;
+        Random rand = new Random();
+
+        for (int i = 0; i < 32000; i++) {
+
+            try {
+                b = rand.nextInt();
+                c = rand.nextInt();
+                a = 12345 / (b/c);
+            } catch (ArithmeticException e) {
+                System.out.println("Division by zero");
+                a = 0;
+            }
+
+            System.out.println("a: " + a);
+
+        }
+    }
+}
