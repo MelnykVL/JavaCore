99a5e7f1efe945ec59becdbfc48a0f69da008266
MelnykVL <w.vladyslavmelnyk@gmail.com>
2021-07-02 18:28:55 +0300

    Start chapter15

---
+++ src/main/java/com/melnykvl/javacore/chapter15/LambdaDemo.java
@@ -0,0 +1,23 @@
+package main.java.com.melnykvl.javacore.chapter15;
+
+interface MyNumber {
+    double getValue();
+}
+
+public class LambdaDemo {
+    public static void main(String[] args) {
+
+        MyNumber myNum;
+        myNum = () -> 123.45;
+
+        System.out.println("Static value: " + myNum.getValue());
+
+        myNum = () -> Math.random() * 100;
+
+        System.out.println("Random value 1: " + myNum.getValue());
+        System.out.println("Random value 2: " + myNum.getValue());
+
+//        myNum = () -> "123.03"; !----Error----!
+
+    }
+}
