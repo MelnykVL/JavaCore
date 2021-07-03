f233116931b1d8507272bd1b2d53e11c6c45ba71
MelnykVL <w.vladyslavmelnyk@gmail.com>
2021-07-03 18:54:11 +0300

    Added DirList.java

---
+++ src/main/java/com/melnykvl/javacore/chapter20/DirList.java
@@ -0,0 +1,28 @@
+package main.java.com.melnykvl.javacore.chapter20;
+
+import java.io.File;
+
+public class DirList {
+    public static void main(String[] args) {
+
+        String dirName = "src/main/java/com/melnykvl/javacore/chapter20";
+        File f1 = new File(dirName);
+
+        if (f1.isDirectory()) {
+            System.out.println("Каталог " + dirName);
+            String[] s = f1.list();
+
+            for (int i = 0; i < s.length; i++) {
+                File f = new File(dirName + "/" + s[i]);
+
+                if (f.isDirectory())
+                    System.out.println(s[i] + " являеться каталогом");
+                else
+                    System.out.println(s[i] + " являеться файлом");
+            }
+        } else {
+            System.out.println(dirName + " не являеться каталогом");
+        }
+
+    }
+}
