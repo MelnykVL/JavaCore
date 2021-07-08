d7d7f62cdb6052dc46383f6a88d6b4a1e8c743c9
MelnykVL <w.vladyslavmelnyk@gmail.com>
2021-07-08 15:35:21 +0300

    Added URLDemo.java

---
+++ src/main/java/com/melnykvl/javacore/chapter22/URLDemo.java
@@ -0,0 +1,21 @@
+package main.java.com.melnykvl.javacore.chapter22;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class URLDemo {
+    public static void main(String[] args) throws MalformedURLException, IOException {
+
+        URL hp = new URL("http://www.HerbSchildt.com/Articles");
+
+        System.out.println("Protocol: " + hp.getProtocol());
+        System.out.println("Port: " + hp.getPort());
+
+        System.out.println("Host: " + hp.getHost());
+        System.out.println("File: " + hp.getFile());
+        System.out.println("Full form: " + hp.toExternalForm());
+
+    }
+}
